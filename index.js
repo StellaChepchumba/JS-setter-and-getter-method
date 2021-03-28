a515ed62a38f63876c095c90d55addebb68b5469
stella4545 <stellachepchumbah@gmail.com>
2021-03-28 15:59:15 +0300

    js

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function kiosk(){
+  var fruit=fruits["peach","apples","tomatoes"];
+  set (fruits[1],"mango");
+  {
+    this.fruits=[1],"mango";
+  };
+  get ;this.fruits[1]; (){=>
+    return this.fruit;
+  }
+  };
+  console.log(fruits.fruits);
